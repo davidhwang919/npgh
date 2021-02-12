b2a4ff26943939fe41a35250836c4d25fa17d65e
David <davidhwang919@gmail.com>
2021-02-12 14:12:11 -0600

    awesome changes

--- npgh_project/urls.py
+++ npgh_project/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('blog/', include('blog.urls')),
     path('about', views.about, name='about'),
+    path('help', views.help, name='help'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
